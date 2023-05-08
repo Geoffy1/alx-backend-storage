75058843ad665a080fe991f098cd46eb4885c42c
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-05-09 02:53:10 +0300

    Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,39 +1,47 @@
 #!/usr/bin/env python3
 
 from pymongo import MongoClient
-from collections import Counter
 
 
 def print_nginx_request_logs(nginx_collection):
-    '''Database: logs
+    """Database: logs
     Collection: nginx
-    '''
-    # Print the total number of logs
+    """
     print('{} logs'.format(nginx_collection.count_documents({})))
-
-    # Print the number of requests by HTTP method
-    print('HTTP Methods:')
+    print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
         req_count = nginx_collection.count_documents({'method': method})
-        print('\t{}: {}'.format(method, req_count))
+        print('\tmethod {}: {}'.format(method, req_count))
 
-    # Print the number of status check requests
     status_checks_count = nginx_collection.count_documents({'method': 'GET', 'path': '/status'})
-    print('{} status check requests'.format(status_checks_count))
+    print('{} status checks'.format(status_checks_count))
 
-    # Print the top 10 most present IPs in the collection
-    ips = Counter(log['ip'] for log in nginx_collection.find())
-    print('Top 10 IPs:')
-    for ip, count in ips.most_common(10):
-        print('\t{}: {}'.format(ip, count))
+    print('IPs:')
+    pipeline = [
+        {
+            '$group': {
+                '_id': '$remote_addr',
+                'count': {'$sum': 1},
+            },
+        },
+        {
+            '$sort': {'count': -1},
+        },
+        {
+            '$limit': 10,
+        },
+    ]
+    for ip_doc in nginx_collection.aggregate(pipeline):
+        print('\t{}: {}'.format(ip_doc['_id'], ip_doc['count']))
 
 
 def run():
-    '''Display various attributes
-    '''
+    """Display various attributes
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
+    nginx_collection = client.logs.nginx
+    print_nginx_request_logs(nginx_collection)
 
 
 if __name__ == '__main__':
