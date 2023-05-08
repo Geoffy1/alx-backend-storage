7eba604247d0927bea2c8f9362ef818956fea104
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-05-09 02:11:45 +0300

    Update 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,10 +1,12 @@
-import pymongo
+#!/usr/bin/env python3
+'''//Write a Python function that lists all documents in a collection:
+//Prototype: def list_all(mongo_collection):
+// an empty list if no document in the collection
+//mongo_collection will be the pymongo collection object
+'''
+
 
 def list_all(mongo_collection):
-    # Use the find() method to retrieve all documents in the collection
-    cursor = mongo_collection.find({})
-    # Convert the cursor to a list of documents
-    documents = list(cursor)
-    # Return the list of documents, or an empty list if there are no documents
-    return documents if documents else []
-
+    '''Lists all documents in a collection.
+    '''
+    return [doc for doc in mongo_collection.find()]
