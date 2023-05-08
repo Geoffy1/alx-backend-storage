2f9bb1731c9e192b8a5fea05d2c7407dedc92ece
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-05-09 02:17:16 +0300

    Update 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,8 +1,15 @@
-import pymongo
+#!/usr/bin/env python3
+'''Task 10
+'''
+
 
 def update_topics(mongo_collection, name, topics):
-    # Use the update_many() method to update all documents with the given name
-    result = mongo_collection.update_many({'name': name}, {'$set': {'topics': topics}})
-    # Return the number of documents updated
-    return result.modified_count
-
+    '''Prototype: def update_topics(mongo_collection, name, topics):
+    mongo_collection will be the pymongo collection object
+    name (string) will be the school name to update
+    topics (list of strings) will be the list of topics approached in the school
+    '''
+    mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
+    )
