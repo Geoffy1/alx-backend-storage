884698d12ad194d67baf098c69f46fd46f7a8720
Geoffy1 <geojogu@gmail.com>
2023-05-08 16:45:06 -0700

    task 102

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""adding the top 10 of the most present IPs in the collection nginx of the database logs
+"""adding the top 10 of the most present IPs
+in the collection nginx of the database logs
 """
 
 from pymongo import MongoClient
