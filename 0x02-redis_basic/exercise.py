4a1bc880a8cc412004bc90759c3c123f0d10146e
Geoffy1 <geojogu@gmail.com>
2023-05-10 19:58:18 +0000

    my reddis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,16 +2,36 @@
 """A module for using the Redis NoSQL data storage.
 """
 
-import uuid
-import redis
-
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @staticmethod
+    def count_calls(method: Callable) -> Callable:
+        @wraps(method)
+        def wrapper(self, *args, **kwargs):
+            key = method.__qualname__
+            self._redis.incr(key)
+            return method(self, *args, **kwargs)
+        return wrapper
+
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        return self.get(key, fn=lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        return self.get(key, fn=int)
