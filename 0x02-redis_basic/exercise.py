652769b36adcbd8babf2edd5702957fab5576b30
Geoffy1 <geojogu@gmail.com>
2023-05-10 19:47:32 +0000

    my reddis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""A module for using the Redis NoSQL data storage.
+"""
+
+import uuid
+import redis
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
